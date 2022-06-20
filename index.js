29862cc4c010200c5bcb3b6eff2b4f2260cf6207
unknown <cabaraul@gmail.com>
2022-06-20 17:46:25 -0400

    First game

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(Path, leftPosition, bottomPosition ){
+let imgName = document.createElement('img')
+imgName.src = Path
+ imgName.style.position = 'fixed'
+imgName.style.left = leftPosition
+imgName.style.bottom = bottomPosition
+document.body.append(imgName)
+}
+
+function newItem(Path,leftPosition,bottomPosition  ){
+let item = document.createElement('img')
+item.src = Path
+item.position = 'fixed'
+item.left = leftPosition
+item.bottom = bottomPosition
+document.body.append(item)
+item.addEventListener('dblclick', function(){
+    item.remove()
+})
+
+// function Background(Path, leftPosition, bottomPosition ){
+// let imgName = document.createElement('img')
+// imgName.src = Path
+//  imgName.style.position = 'fixed'
+// imgName.style.left = leftPosition
+// imgName.style.bottom = bottomPosition
+// document.body.append(imgName)
+// }
+
+// Background('assets/grass.png', '1366', '657')
+
+console.log(window.innerWidth, window.innerHeight)
+
+
+
+}
+
+newImage( 'assets/green-character.gif', '100px', '100px' )
+newImage( 'assets/pine-tree.png', '450px', '200px')
+newImage( 'assets/pillar.png', '350px', '100px')
+newImage( 'assets/tree.png', '200px', '300px')
+newImage( 'assets/crate.png', '150px', '200px')
+newImage( 'assets/well.png', '500px', '425px')
+
+
+newItem('assets/sword.png', '165px', '185px')
+newItem('assets/sheild.png', '500px', '405px')
+
+newItem('assets/staff.png', '600px', '100px')
+
+
+
